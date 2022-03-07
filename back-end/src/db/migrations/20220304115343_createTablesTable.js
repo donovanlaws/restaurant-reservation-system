0007c9e85b845c54cd96b6cb4b549042e9f49968
Donovan Laws <donovan.laws@gmail.com>
2022-03-07 13:49:22 -0700

    Added defaults for status on reservations and tables

--- back-end/src/db/migrations/20220304115343_createTablesTable.js
+++ back-end/src/db/migrations/20220304115343_createTablesTable.js
@@ -4,7 +4,7 @@ exports.up = function (knex) {
         table.increments("table_id").primary();
         table.string("table_name");
         table.integer("capacity");
-        table.string("status");
+        table.string("status").defaultTo("free");
         table.integer("reservation_id").references("reservation_id").inTable("reservations").onDelete("set null");
         table.timestamps(true, true);
     });
